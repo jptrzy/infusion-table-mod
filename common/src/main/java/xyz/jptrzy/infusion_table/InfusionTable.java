31ee3adbe3e308b09582f8eb82584fa36646d976
Jakub Trzeciak <jp3@jptrzy.xyz>
2026-09-11 11:54:36 +0200

    refactor: migrate to 1.21 data regarding block breaking

--- common/src/main/java/xyz/jptrzy/infusion_table/InfusionTable.java
+++ common/src/main/java/xyz/jptrzy/infusion_table/InfusionTable.java
@@ -31,7 +31,7 @@ public class InfusionTable {
     public static final Registrar<BlockEntityType<?>> BLOCK_ENTITY_TYPE_REGISTRAR = MANAGER.get().get(RegistryKeys.BLOCK_ENTITY_TYPE);
 
     public static final Identifier INFUSION_TABLE_ID = Identifier.of(MOD_ID, "infusion_table");
-    public static RegistrySupplier<Block> INFUSION_TABLE_BLOCK = BLOCK_REGISTRAR.register(INFUSION_TABLE_ID, () -> new InfusionTableBlock(AbstractBlock.Settings.create().strength(4.0f).luminance(new BlockLuminance(7))));
+    public static RegistrySupplier<Block> INFUSION_TABLE_BLOCK = BLOCK_REGISTRAR.register(INFUSION_TABLE_ID, () -> new InfusionTableBlock(AbstractBlock.Settings.create().strength(4f, 1f).requiresTool().luminance(new BlockLuminance(7))));
 
     public static RegistrySupplier<BlockItem> INFUSION_TABLE_BLOCK_ITEM = ITEM_REGISTRAR.register(INFUSION_TABLE_ID, () -> new BlockItem(INFUSION_TABLE_BLOCK.get(), new Item.Settings().arch$tab(ItemGroups.FUNCTIONAL)));
     public static RegistrySupplier<BlockEntityType<InfusionTableBlockEntity>> INFUSION_TABLE_BLOCK_ENTITY = BLOCK_ENTITY_TYPE_REGISTRAR.register(INFUSION_TABLE_ID, () -> BlockEntityType.Builder.create(InfusionTableBlockEntity::new, INFUSION_TABLE_BLOCK.get()).build(null));
